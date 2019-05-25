368445513fd9e6e34ad6c68793595613ea003c4e
Sinister6 <naman.chandel_cs17@gla.ac.in>
2019-05-25 19:25:59 +0530

    MLAIPractical10.py

---
+++ MLAIPractical10.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+darr = np.array([[1,2,3],[4,5,6]])
+
+print("drr.ndim",darr.ndim)
+
+print(darr.shape)
+
+print(darr.size)
+
+print(darr.dtype)
+
+print(darr)
+
+print(darr[0,1])
+
+print(darr[0 ])
+
+print(darr[:,0])
+
+print(darr[1,:])
