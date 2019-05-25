fb969bf55ba7a929f94d7785bd35161c8ab69940
Sinister6 <naman.chandel_cs17@gla.ac.in>
2019-05-25 19:25:48 +0530

    MLAIPractical09.py

---
+++ MLAIPractical09.py
@@ -0,0 +1,31 @@
+import numpy as np 
+
+list1 = [1,2,3]
+
+print("list*4=",list1*4)
+
+nparr = np.array([1,2,3])
+print(nparr)
+
+print("nparr *4 ",nparr*4)
+print("nparra+4",nparr+4)
+
+print(nparr/2)
+print(nparr/2.0)
+print(nparr//2.0)
+print(nparr//2)
+
+
+a=10
+print(type(a))
+
+a = 12.5
+print(type(a))
+
+print(type(nparr))
+
+print("nparr.dtype==",nparr.dtype)
+
+print(nparr.size)
+
+
