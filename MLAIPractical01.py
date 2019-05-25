6cadf0634e77bf3891b71917c2aeb25f601987b3
Sinister6 <naman.chandel_cs17@gla.ac.in>
2019-05-25 19:24:24 +0530

    MLAIPractical01.py

---
+++ MLAIPractical01.py
@@ -0,0 +1,15 @@
+import numpy
+import matplotlib
+import pandas
+import sklearn
+import scipy
+import seaborn
+
+print(numpy.__version__)
+print(matplotlib.__version__)
+print(pandas.__version__)
+print(sklearn.__version__)
+print(scipy.__version__)
+print(seaborn.__version__)
+
+
